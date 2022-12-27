dfd181f62016e9d6d35efe6de06f502c24d3937f
MaksimYakovenko <103666491+MaksimYakovenko@users.noreply.github.com>
2022-12-28 01:09:05 +0200

    Add files via upload

---
+++ T03_inheritance/l3_3_1_2.py
@@ -0,0 +1,112 @@
+import math
+from l3_3_1 import Figure
+
+
+class Triangle(Figure):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.name = 'Triangle'
+
+    def dim(self):
+        return 2
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    def square(self):
+        p = self.perimeter() / 2
+        s = (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
+        return s
+
+
+class Rectangle(Figure):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.name = 'Rectangle'
+
+    def dim(self):
+        return 2
+
+    def perimeter(self):
+        return 2 * (self.a + self.b)
+
+    def square(self):
+        return self.a * self.b
+
+
+class Trapeze(Figure):
+    def __init__(self, a, b, c, d):
+        self.name = 'Trapeze'
+
+        coef = []
+        coef.append(a)
+        coef.append(b)
+        coef.append(c)
+        coef.append(d)
+
+        for i in range(len(coef) - 1):
+            for j in range(len(coef) - 1 - i):
+                if coef[j] > coef[j + 1]:
+                    coef[j], coef[j + 1] = coef[j + 1], coef[j]
+
+        a, b, c, d = coef
+        self.a = d
+        self.b = c
+        self.c = b
+        self.d = a
+
+    def dim(self):
+        return 2
+
+    def perimeter(self):
+        p = self.a + self.b + self.c + self.d
+        return p
+
+    def square(self):
+        if self.a > self.b and self.c > self.d:
+            S = (self.a + self.b) / 2
+            s = (self.c ** 2 - (((self.a - self.b) ** 2 + self.c ** 2 - self.d ** 2) / (2 * (self.a - self.b)))) ** 0.5
+            return S * s
+        else:
+            p2 = self.perimeter() / 2
+            s = ((p2 - self.a) * (p2 - self.b) * (p2 - self.c) * (p2 - self.d)) ** 0.5
+            return s
+
+
+class Parallelogram(Figure):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.name = 'Parallelogram'
+
+    def dim(self):
+        return 2
+
+    def perimeter(self):
+        return 2 * (self.a + self.b)
+
+    def square(self):
+        return self.a * self.c
+
+
+class Circle(Figure):
+    def __init__(self, a):
+        self.a = a
+        self.name = 'Circle'
+
+    def dim(self):
+        return 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.a
+
+    def square(self):
+        return math.pi * (self.a ** 2)
+
+
+
+
---
+++ T03_inheritance/l3_3_1_3.py
@@ -0,0 +1,159 @@
+import math
+from l3_3_1_2 import Triangle, Rectangle, Circle
+from l3_3_1 import Figure
+
+
+class TriangularPyramid(Figure):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.name = 'Triangular Pyramid'
+
+    def dim(self):
+        return 3
+
+    def squareSurface(self):
+        return 0.5 * self.a * 3 * self.b
+
+    def squareBase(self):
+        p = (self.a * 3) / 2
+        return (p * ((p - self.a) ** 3)) ** 0.5
+
+    def square(self):
+        return self.squareSurface() + self.squareBase()
+
+    def height(self):
+        return self.b
+
+    def _volume(self):
+        return 1/3 * (self.squareBase() * self.b)
+
+
+class TriangularPrism(Triangle):
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c)
+        self.d = d
+        self.name = 'Triangular Prism'
+
+    def dim(self):
+        return 3
+
+    def perimeter(self):
+        return self.d * 3 + 2 * super().perimeter()
+
+    def squareSurface(self):
+        return super().perimeter() * self.d
+
+    def squareBase(self):
+        return super().square()
+
+    def square(self):
+        return 2 * self.squareBase() + self.squareSurface()
+
+    def height(self):
+        return self.d
+
+    def _volume(self):
+        return self.squareBase() * self.height()
+
+
+class QuadrangularPyramid(Rectangle):
+    def __init__(self, a, b, c):
+        super().__init__(a, b)
+        self.c = c
+        self.name = 'Quadrangular Pyramid'
+
+    def dim(self):
+        return 3
+
+    def squareSurface(self):
+        return super().perimeter() * self.c
+
+    def squareBase(self):
+        return super().square()
+
+    def square(self):
+        return self.squareBase() + self.squareSurface()
+
+    def height(self):
+        return self.c
+
+    def _volume(self):
+        return 1/3 * (self.squareBase() * self.c)
+
+
+class RectangularParallelepiped(Rectangle):
+    def __init__(self, a, b, c):
+        super().__init__(a, b)
+        self.c = c
+        self.name = 'Rectangular Parallelepiped'
+
+    def dim(self):
+        return 3
+
+    def perimeter(self):
+        return (self.a + self.b + self.c) * 4
+
+    def squareSurface(self):
+        return 2 * (self.a + self.b) * self.c
+
+    def squareBase(self):
+        return self.a * self.b
+
+    def square(self):
+        return 2 * self.squareBase() + self.squareSurface()
+
+    def height(self):
+        return self.c
+
+    def _volume(self):
+        return self.a * self.b * self.c
+
+
+class Cone(Circle):
+    def __init__(self, a, b):
+        super().__init__(a)
+        self.b = b
+        self.name = 'Cone'
+
+    def dim(self):
+        return 3
+
+    def squareSurface(self):
+        line = math.sqrt(self.a ** 2 + self.b ** 2)
+        return math.pi * line * self.a
+
+    def squareBase(self):
+        return super().square()
+
+    def square(self):
+        return self.squareBase() + self.squareSurface()
+
+    def height(self):
+        return self.b
+
+    def _volume(self):
+        return 1/3 * (super().square() * self.b)
+
+
+class Ball(Figure):
+    def __init__(self, a):
+        self.a = a
+        self.name = 'Ball'
+
+    def dim(self):
+        return 3
+
+    def square(self):
+        return 4 * math.pi * (self.a ** 2)
+
+    def height(self):
+        return 2 * self.a
+
+    def _volume(self):
+        return 4/3 * (math.pi * self.a ** 3)
+
+
+if __name__ == '__main__':
+    q = QuadrangularPyramid(14, 18, 22)
+    print(q._volume())
